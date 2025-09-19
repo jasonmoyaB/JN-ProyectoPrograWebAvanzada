806124094d3632655211dd0c1b05870697962084
jason moya brenes <jason.moyabre.es@gmail.com>
2025-09-19 12:52:45 -0600

    my first commit

---
+++ JN_ProyectoPrograAvanzadaWeb-G1/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+using System.Diagnostics;
+using JN_ProyectoPrograAvanzadaWeb_G1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JN_ProyectoPrograAvanzadaWeb_G1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Main()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ JN_ProyectoPrograAvanzadaWeb-G1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JN_ProyectoPrograAvanzadaWeb_G1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
